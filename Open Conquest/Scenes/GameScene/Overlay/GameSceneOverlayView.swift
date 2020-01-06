ab5499a4d31d8ea6a21a09e55e74a728ba66b035
Zach Wild <zacharyjwild@gmail.com>
2020-01-06 18:28:20 -0500

    Remove navbar, kingdom label, and old resource indicators

--- Open Conquest/Scenes/GameScene/Overlay/GameSceneOverlayView.swift
+++ Open Conquest/Scenes/GameScene/Overlay/GameSceneOverlayView.swift
@@ -13,57 +13,30 @@ import PureLayout
 class GameSceneOverlayView: UIView {
 
     let screenSize = UIScreen.main.bounds
-
-    var uiNavbar: UIImageView = UIImageView()
-    var uiKingdomLabel: UILabel = UILabel()
-    var uiGold: UIView = UIView()
-    var uiPower: UIView = UIView()
-    var uiWood: UIView = UIView()
-    var uiStone: UIView = UIView()
-    var uiOre: UIView = UIView()
     
     var goldTicker = ResourceTickerView()
     var powerTicker = ResourceTickerView()
 
     func setupUI() {
-        // setup navbar
-        let uiNavbarHeight = screenSize.height/10
-        let uiNavbarWidth = screenSize.width
-        uiNavbar.image = UIImage(named: "navbar-background")
-        uiNavbar.alpha = 0
-        uiNavbar.autoSetDimension(.height, toSize: uiNavbarHeight)
-        uiNavbar.autoSetDimension(.width, toSize: uiNavbarWidth)
-        addSubview(uiNavbar)
-        uiNavbar.autoPinEdge(toSuperviewMargin: .top)
-
-        // setup kingdom name
-        uiKingdomLabel.text = "Kingdom"
-        uiKingdomLabel.textAlignment = NSTextAlignment.center
-        uiKingdomLabel.textColor = UIColor.white
-        uiKingdomLabel.font = UIFont(name: "Chalkduster", size: 18)
-        uiNavbar.addSubview(uiKingdomLabel)
-        uiKingdomLabel.autoPinEdge(.top, to: .top, of: uiNavbar, withOffset: 2)
-        uiKingdomLabel.autoPinEdge(.left, to: .left, of: uiNavbar)
-        uiKingdomLabel.autoPinEdge(.right, to: .right, of: uiNavbar)
-        
         // setup resources
-        let resourceBackgroundHeight = uiNavbarHeight/2.2
-        let resourceBackgroundWidth = uiNavbarWidth/4
-        let widthOffset = (resourceBackgroundWidth)/10
+        let resourceBackgroundHeight = screenSize.height/22.0
+        let resourceBackgroundWidth = screenSize.width/4
+        let resourceHeightOffset = resourceBackgroundHeight
+        let resourceWidthOffset = (resourceBackgroundWidth)/10
         let resourceBackgroundNavbarOffset: CGFloat = -5
         
         goldTicker.autoSetDimension(.height, toSize: resourceBackgroundHeight)
         goldTicker.autoSetDimension(.width, toSize: resourceBackgroundWidth)
         addSubview(goldTicker)
-        goldTicker.autoPinEdge(.bottom, to: .bottom, of: uiNavbar, withOffset: resourceBackgroundNavbarOffset)
-        goldTicker.autoPinEdge(.left, to: .left, of: self, withOffset: widthOffset)
+        goldTicker.autoPinEdge(.top, to: .top, of: self, withOffset: resourceHeightOffset)
+        goldTicker.autoPinEdge(.left, to: .left, of: self, withOffset: resourceWidthOffset)
         goldTicker.setupUI(resource: ResourceEnum.gold)
         
         powerTicker.autoSetDimension(.height, toSize: resourceBackgroundHeight)
         powerTicker.autoSetDimension(.width, toSize: resourceBackgroundWidth)
         addSubview(powerTicker)
         powerTicker.autoPinEdge(.top, to: .bottom, of: goldTicker, withOffset: resourceBackgroundNavbarOffset)
-        powerTicker.autoPinEdge(.left, to: .left, of: self, withOffset: widthOffset)
+        powerTicker.autoPinEdge(.left, to: .left, of: self, withOffset: resourceWidthOffset)
         powerTicker.setupUI(resource: ResourceEnum.power)
     }
 }
