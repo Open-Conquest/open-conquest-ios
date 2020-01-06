ba0b8bff495c91a2685d441bec0c87a062e16866
Zach Wild <zacharyjwild@gmail.com>
2020-01-06 12:32:08 -0600

    Add UIView extension for gradient

--- Open Conquest/Extensions/UIViewExtensions.swift
+++ Open Conquest/Extensions/UIViewExtensions.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIView {
+    func setGradientBackground(colorOne: UIColor, colorTwo: UIColor) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.endPoint = CGPoint(x: 0, y: 0)
+        
+        layer.insertSublayer(gradientLayer, at: 0)
+        gradientLayer.display()
+    }
+}
