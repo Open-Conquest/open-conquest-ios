b0b496781802607821c6967c3b8a5a8cf833ddfa
Zach Wild <zacharyjwild@gmail.com>
2020-03-12 17:13:30 -0400

    Remove get entities game notifications

--- Open Conquest/Game/Shared/Notifications/GameNotifications.swift
+++ Open Conquest/Game/Shared/Notifications/GameNotifications.swift
@@ -24,17 +24,7 @@ enum GameNotifications: String
     
     case LoadGame = "game-load-game"
     
-    case TryGetArmies = "game-try-get-armies"
-    case TryGetCities = "game-try-get-cities"
-    case TryGetMap = "game-try-get-map"
-    case TryGetMarches = "game-try-get-marches"
-    case TryGetUsers = "game-try-get-users"
-    
-    case DidGetArmies = "game-did-get-armies"
-    case DidGetCities = "game-did-get-cities"
-    case DidGetMap = "game-did-get-map"
-    case DidGetMarches = "game-did-get-marches"
-    case DidGetUsers = "game-did-get-users"
+    case TryGetWorld = "game-try-get-world"
 }
 
 extension Notification.Name
@@ -53,17 +43,7 @@ extension Notification.Name
     
     static let GameLoadGame = Notification.Name(rawValue: GameNotifications.LoadGame.rawValue)
     
-    static let GameTryGetArmies = Notification.Name(rawValue: GameNotifications.TryGetArmies.rawValue)
-    static let GameTryGetCities = Notification.Name(rawValue: GameNotifications.TryGetCities.rawValue)
-    static let GameTryGetMap = Notification.Name(rawValue: GameNotifications.TryGetMap.rawValue)
-    static let GameTryGetMarches = Notification.Name(rawValue: GameNotifications.TryGetMarches.rawValue)
-    static let GameTryGetUsers = Notification.Name(rawValue: GameNotifications.TryGetUsers.rawValue)
-    
-    static let GameDidGetArmies = Notification.Name(rawValue: GameNotifications.DidGetArmies.rawValue)
-    static let GameDidGetCities = Notification.Name(rawValue: GameNotifications.DidGetCities.rawValue)
-    static let GameDidGetMap = Notification.Name(rawValue: GameNotifications.DidGetMap.rawValue)
-    static let GameDidGetMarches = Notification.Name(rawValue: GameNotifications.DidGetMarches.rawValue)
-    static let GameDidGetUsers = Notification.Name(rawValue: GameNotifications.DidGetUsers.rawValue)
+    static let GameTryGetWorld = Notification.Name(rawValue: GameNotifications.TryGetWorld.rawValue)
 }
 
 
--- Open Conquest/Game/Shared/Notifications/GamePublisher.swift
+++ Open Conquest/Game/Shared/Notifications/GamePublisher.swift
@@ -13,20 +13,4 @@ class GamePublisher: Publisher {
     override init() {
         super.init()
     }
-    
-//    // asks each component to load themselves
-//    func loadGame(username: String) {
-//        print("Game publishing GameLoadGame event...")
-//        let name = Notification.Name.GameLoadGame
-//        let data : [AnyHashable: Any]? = ["username": username]
-//        let notification = notificationFactory.buildNotification(name: name, data: data)
-//        post(notification: notification)
-//    }
-//
-//    func gameLoaded() {
-//        print("Game publishing GameGameLoaded event...")
-////        let name = Notification.Name.GameGameLoaded
-////        let notification = notificationFactory.buildNotification(name: name, data: nil)
-////        post(notification: notification)
-//    }
 }
